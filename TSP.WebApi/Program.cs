dd54c6c8622720b4e8659bc34c3a8556a68c6b93
koen de vulder <koen.de.vulder@tabaknatie.be>
2024-12-12 16:12:07 +0100

    feat: added dark mode for swagger 😶‍🌫️

--- TSP.WebApi/Program.cs
+++ TSP.WebApi/Program.cs
@@ -32,11 +32,16 @@ builder.Services.AddCors(options =>
 
 var app = builder.Build();
 
+app.UseStaticFiles();
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(options =>
+    {
+        options.InjectStylesheet("/swagger-ui/custom.css");
+    });
 }
 
 // Use CORS before other middleware
