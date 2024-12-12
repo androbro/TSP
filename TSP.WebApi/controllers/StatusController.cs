0a80bd876569888d5104460bf62ed5ed79363f2e
koen de vulder <koen.de.vulder@tabaknatie.be>
2024-12-12 15:11:56 +0100

    feat: set up controller

---
+++ TSP.WebApi/controllers/StatusController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TSP.WebApi.controllers;
+
+[ApiController]
+[Route("TSP/[controller]")]
+public class StatusController: Controller
+{
+        public StatusController(){}
+        
+        [HttpGet]
+        public IActionResult Get()
+        {
+            
+            return Ok();
+        }
+
+}
